7c3ad39422de7ba091a81e49c07ecb166bf4286a
Frédéric Simonis <simonis@in.tum.de>
2019-04-08 15:01:41 +0200

    seissol: extend package

--- var/spack/repos/builtin/packages/seissol/package.py
+++ var/spack/repos/builtin/packages/seissol/package.py
@@ -29,33 +29,55 @@ class Seissol(SConsPackage):
     # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://www.seissol.org"
     url      = "https://github.com/SeisSol/SeisSol"
-    #git      = "https://github.com/SeisSol/SeisSol.git"
+    git      = "https://github.com/SeisSol/SeisSol.git"
 
-    # FIXME: Add proper versions and checksums here.
     version('master', branch='master', submodules=True)
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
+    variant('order', default='2', description='convergence order of the ADER-DG method', values=('2', '3', '4', '5', '6', '7', '8'), multi=False)
+    variant('mpi', default=True)
+    variant('openmp', default=True)
+    variant('precision', default='d', values=('s', 'd'), description='Floating point precision to use.')
+    variant('arch', default='dnoarch', values=('snoarch', 'dnoarch', 'swsm', 'dwsm', 'ssnb', 'dsnb', 'sknc', 'dknc', 'shsw', 'dhsw', 'sknl', 'dknl', 'sskx', 'dskx'), multi=True)
+    variant('asagi', default=True, description="use asagi for material input")
+
     depends_on('python@2.7:')
-    depends_on('hdf5~shared+mpi+fortran+threadsafe')
-    depends_on('netcdf+mpi~shared')
-    depends_on('mpi')
     depends_on('parmetis')
     depends_on('libxsmm')
     depends_on('cmake')
-
-    def patch(self):
-        """ We need to setup the configuration by patching build/options/supermuc_mac_cluster.py """
-        optsfile=os.path.join(self.package_dir, "build", "options", "supermuc_mac_cluster")
-        filter_file(r"^arch.\+", r"arch  = '{}'".format(self.arch), optsfile)
-
-        if self.debug:
-            filter_file(r"^compileMode.\+", r"compileMode='debug'", optsfile)
-
-
+    depends_on('memkind')
+    depends_on('mpi', when="+mpi")
+    depends_on('hdf5 +shared +fortran ~mpi', when="~mpi")
+    depends_on('hdf5 +shared +fortran +mpi', when="+mpi")
+    depends_on('netcdf +shared ~mpi', when="~mpi")
+    depends_on('netcdf +shared +mpi', when="+mpi")
+    depends_on('asagi ~mpi', when="+asagi ~mpi")
+    depends_on('asagi +mpi', when="+asagi +mpi")
+            
     def build_args(self, spec, prefix):
-        # FIXME: Add arguments to pass to build.
-        # FIXME: If not needed delete this function
+        import os
         args = []
-        args.append("buildVariablesFile=build/options/supermuc_mac_cluster.py")
+        #  args.append("buildVariablesFile=build/options/supermuc_mac_cluster.py")
+        if "+debug" in spec:
+            args.append("compileMode=debug")
+        else:
+            args.append("compileMode=release")
+
+        if "+mpi" in spec and "+openmp" in spec:
+            args.append("parallelization=hybrid")
+        elif "+mpi" in spec:
+            args.append("parallelization=mpi")
+        elif "+openmp" in spec:
+            args.append("parallelization=omp")
+        else:
+            args.append("parallelization=none")
+
+        args.append("arch={}".format(spec.variants["arch"].value[0]))
+        args.append("order={}".format(spec.variants["order"].value[0]))
+        args.append("memkindDir={}".format(os.path.join(spec['memkind'].prefix, "lib")))
+        args.append("netcdf=yes")
+        args.append("netcdfDir={}".format(os.path.join(spec['netcdf'].prefix, "lib")))
+        args.append("hdf5=yes")
+        args.append("hdf5Dir={}".format(os.path.join(spec['hdf5'].prefix, "lib")))
+        args.append("metis=yes")
+        args.append("metisDir={}".format(os.path.join(spec['metis'].prefix, "lib")))
         return args
