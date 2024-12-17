b1940efc059e64093d167fcef812fb20e238f9d0
Frédéric Simonis <simonisfrederic@gmail.com>
2024-12-17 17:12:58 +0100

    Enforce correct variants in kokkos based on compiler

--- var/spack/repos/builtin/packages/precice/package.py
+++ var/spack/repos/builtin/packages/precice/package.py
@@ -123,6 +123,8 @@ class Precice(CMakePackage, CudaPackage, ROCmPackage):
         depends_on("kokkos@4.1:")
         depends_on("kokkos +cuda", when="+cuda")
         depends_on("kokkos +rocm", when="+rocm")
+        depends_on("kokkos +wrapper", when="+cuda %gcc")
+        depends_on("kokkos +cuda_lambda+cuda_constexpr", when="+cuda %clang")
 
     # We require C++14 compiler support
     conflicts("%gcc@:4")
